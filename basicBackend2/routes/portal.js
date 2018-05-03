88174c8f36e10d7ede0bada8faa9ddbb7d0cb2bc
hamuPP <oppaikyouma@163.com>
2018-05-03 08:29:21 +0800

    update

--- basicBackend2/routes/portal.js
+++ basicBackend2/routes/portal.js
@@ -36,53 +36,66 @@ function VerbGet(reqq,ress){
 
     //TODO 不该用baseUrl
 
-    switch(reqq.path){
-        case '/portal':
-            ress.send('yeeeeeh');
-            break;
-        case '/portal/page':
-             setTimeout(function(){
-                ress.redirect('http://www.baidu.com');
-            },3000);
-            break;
-        case '/treeMe':
-            fs.readFile('routes/portal/enterprisesTree.json', 'utf8', (err, data) => {
-                if (err) throw err;
-                ress.send(data)
-            });
-            break;
-        case '/backlogsData':
-            fs.readFile('routes/portal/backlogsData.json', 'utf8', (err, data) => {
-                if (err) throw err;
-                ress.send(data)
-            });
-            break;
-        case '/backlogsTagData':
-            fs.readFile('routes/portal/backlogsTagData.json', 'utf8', (err, data) => {
-                if (err) throw err;
-                ress.send(data)
-            });
-            break;
-        case '/resources/admin/functions':
-            fs.readFile('routes/portal/functions.json', 'utf8', (err, data) => {
-                if (err) throw err;
-                ress.send(data)
-            });
-            break;
+    if( /\/(backlogs).+(tags)/.test(reqq.path)){
+        fs.readFile('routes/portal/backlogsTagData.json', 'utf8', (err, data) => {
+            if (err) throw err;
+            ress.send(data)
+        });
+    }else if( /\/backlogs/.test(reqq.path)){
+        fs.readFile('routes/portal/backlogsData.json', 'utf8', (err, data) => {
+            if (err) throw err;
+            ress.send(data)
+        });
+    } else {
+        switch(reqq.path){
+            case /\/backlogs/.test(reqq.path):
+                console.log('mmmmmm')
+                fs.readFile('routes/portal/backlogsData.json', 'utf8', (err, data) => {
+                    if (err) throw err;
+                    ress.send(data)
+                });
+                break;
+            case '/portal':
+                ress.send('yeeeeeh');
+                break;
+            case '/portal/page':
+                setTimeout(function(){
+                    ress.redirect('http://www.baidu.com');
+                },3000);
+                break;
+            case '/treeMe':
+                fs.readFile('routes/portal/enterprisesTree.json', 'utf8', (err, data) => {
+                    if (err) throw err;
+                    ress.send(data)
+                });
+                break;
+            case '/backlogsTagData':
+                fs.readFile('routes/portal/backlogsTagData.json', 'utf8', (err, data) => {
+                    if (err) throw err;
+                    ress.send(data)
+                });
+                break;
+            case '/resources/admin/functions':
+                fs.readFile('routes/portal/functions.json', 'utf8', (err, data) => {
+                    if (err) throw err;
+                    ress.send(data)
+                });
+                break;
             case '/resources/undefined/functions':
-            fs.readFile('routes/portal/functions.json', 'utf8', (err, data) => {
-                if (err) throw err;
-                ress.send(data)
-            });
-            break;
-        default:
-            res.json({
-                meta:{
-                    code:200,
-                    message:"没有这个接口,但是为了让前端程序正常运行，后端一律返回200" + url
-                }
-            });
-            break;
+                fs.readFile('routes/portal/functions.json', 'utf8', (err, data) => {
+                    if (err) throw err;
+                    ress.send(data)
+                });
+                break;
+            default:
+                res.json({
+                    meta:{
+                        code:200,
+                        message:"没有这个接口,但是为了让前端程序正常运行，后端一律返回200" + url
+                    }
+                });
+                break;
+        }
     }
 }
 
